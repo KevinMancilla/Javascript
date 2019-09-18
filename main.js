cd1f9f7e1ef437b0676bc5c08912606cee0e756a
Kevin Mancilla <kmancilla1@ucol.mx>
2019-09-17 19:34:58 -0500

    Java1

---
+++ main.js
@@ -0,0 +1,37 @@
+/*
+Universidad de colima
+Facultad de telematica
+fundamentos de programacion
+*/
+var nombre = "Kevin Mancilla"
+var edad = "22"
+var esEstudiante= true
+var email = "kmancilla1@ucol.mx"
+var numero1 = 5
+var numero2 = 2
+
+console.log(50 + 10 / 8 + 2)
+console.group((50 + 10) / (8 + 2))
+
+console.log(numero1 + numero2)
+console.log(numero1 - numero2)
+console.log(numero1 * numero2)
+console.log(numero1 / numero2)
+console.log(numero1 % numero2)
+
+console.log(
+    nombre + "tiene" + edad + "años. ¿Es estudiante?" + esEstudiante
+)
+
+//Ejemplo de Conceptos basicos
+console.log(nombre)
+console.log(edad)
+console.log(email)
+console.log ("Hola, mundo!")
+
+edad = "veintidos"
+console.log(edad)
+
+nombre= prompt("¿Como te llamas?")
+
+alert("Hola" + nombre)
